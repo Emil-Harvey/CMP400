64c1ecff4a035f2c877ec5fa5dad2e77c1f8de9b
Emil-Harvey <1800480@uad.ac.uk>
2021-11-23 15:27:36 +0000

    Made UE4 project

--- CMP400_GAN_UE4/Source/CMP400_GAN_UE4/CMP400_GAN_UE4.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "CMP400_GAN_UE4.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CMP400_GAN_UE4, "CMP400_GAN_UE4" );
